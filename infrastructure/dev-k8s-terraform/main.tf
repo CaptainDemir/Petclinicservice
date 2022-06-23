8f051ed693560bcd6d9573876ce15a36843f5f62
captaindemir <captain.demir.89@gmail.com>
2022-06-23 20:22:47 +0000

    added dev-k8s-terraform  for kubernetes infrastructure2

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -204,8 +204,3 @@ output worker-2-ip {
   description = "public ip of the worker-2"
 }
 
-output worker-2-ip {
-  value       = aws_instance.worker-2.public_ip
-  sensitive   = false
-  description = "public ip of the worker-2"
-}
